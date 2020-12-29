31f98b6084aa8d09d76cf6c88055c3728fa0c763
liuwanliang <2467238956@qq.com>
2020-12-29 08:42:23 +0800

    2

---
+++ myshiro_session/src/main/java/com/lwl/shiro_session/web/listener/MySessionListener1.java
@@ -0,0 +1,34 @@
+package com.lwl.shiro_session.web.listener;
+
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.SessionListener;
+
+public class MySessionListener1 implements SessionListener {
+
+    /**
+     * 会话创建时触发
+     * @param session
+     */
+    @Override
+    public void onStart(Session session) {
+        System.out.println("会话创建:"+session.getId());
+    }
+
+    /**
+     * 退出/会话过期时触发
+     * @param session
+     */
+    @Override
+    public void onStop(Session session) {
+        System.out.println("会话停止："+session.getId());
+    }
+
+    /**
+     * 会话过期时触发
+     * @param session
+     */
+    @Override
+    public void onExpiration(Session session) {
+        System.out.println("会话过期："+session.getId());
+    }
+}
