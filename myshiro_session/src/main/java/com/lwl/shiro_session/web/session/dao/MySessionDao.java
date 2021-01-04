c0ec3d695d12dbcdec69e11a081f8cf8e1d79226
liuwanliang <2467238956@qq.com>
2021-01-04 22:55:10 +0800

    2

--- myshiro_session/src/main/java/com/lwl/shiro_session/web/session/dao/MySessionDao.java
+++ myshiro_session/src/main/java/com/lwl/shiro_session/web/session/dao/MySessionDao.java
@@ -3,9 +3,9 @@ package com.lwl.shiro_session.web.session.dao;
 import com.lwl.shiro_session.util.RedisUtils;
 import com.lwl.shiro_session.util.SerializableUtils;
 import org.apache.shiro.session.Session;
-import org.apache.shiro.session.UnknownSessionException;
-import org.apache.shiro.session.mgt.eis.AbstractSessionDAO;
+import org.apache.shiro.session.mgt.ValidatingSession;
 import org.apache.shiro.session.mgt.eis.CachingSessionDAO;
+import redis.clients.jedis.ShardedJedis;
 
 import java.io.Serializable;
 
@@ -14,24 +14,35 @@ public class MySessionDao extends CachingSessionDAO {
     @Override
     protected Serializable doCreate(Session session) {
         Serializable sessionId=generateSessionId(session);
-        RedisUtils.getJedis().set(sessionId.toString(), SerializableUtils.serialize(session));
-
+        ShardedJedis redis = RedisUtils.getJedis();
+        redis.set(sessionId.toString(), SerializableUtils.serialize(session));
+        RedisUtils.closeJedis(redis);
         return sessionId;
     }
 
     @Override
     protected Session doReadSession(Serializable sessionId) {
-        return null;
+        ShardedJedis redis = RedisUtils.getJedis();
+        String sessionStr=redis.get(sessionId.toString());
+
+        return SerializableUtils.deserialize(sessionStr);
     }
 
     @Override
     protected void doUpdate(Session session) {
-
+        if(session instanceof ValidatingSession && !((ValidatingSession) session).isValid()){
+            return;
+        }
+        ShardedJedis redis = RedisUtils.getJedis();
+        redis.set(session.getId().toString(),SerializableUtils.serialize(session));
+        RedisUtils.closeJedis(redis);
     }
 
     @Override
     protected void doDelete(Session session) {
-
+        ShardedJedis redis = RedisUtils.getJedis();
+        redis.del(session.getId().toString());
+        RedisUtils.closeJedis(redis);
     }
 
 
